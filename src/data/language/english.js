e8aa0e82bdd18433772983a752a4b93688d77f35
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-11 18:06:55 +0300

    Add site content

--- src/data/language/english.js
+++ src/data/language/english.js
@@ -1,5 +1,52 @@
 const english = {
-
+    modeSwitch: {
+        darkMode: "DARK MODE",
+        switchLanguage: "Translate to Turkish"
+    },
+    header: {
+        skills: "Skills",
+        projects: "Projects",
+        hireMe: "Hire me"
+    },
+    hero: {
+        text1: "Creative thinker Minimalism lover",
+        text2: "Hi, I’m Almila. I’m a full-stack developer. If you are looking for a Developer who to craft solid and scalable frontend products with great user experiences. Let’s shake hands with me.",
+        hireMeButton: "Hire me"
+    },
+    skills: {
+        title: "Skills"
+    },
+    projects: {
+        title: "Profile",
+        profile: "Profile",
+        aboutMe: "About Me",
+        dateOfBirth: ["Date of Birth", "24.03.1996"],
+        livingIn: ["Living in", "Ankara"],
+        education: ["Education", "Hacettepe Uni. Biology"],
+        preferredRole: ["Preferred role", "Frontend, UI"]
+    },
+    profile: {
+        title: "Projects",
+        viewSite: "View Site",
+        projects: [
+            {
+                title: "Workintech",
+                description: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+            },
+            {
+                title: "Random Jokes",
+                description: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+            },
+            {
+                title: "Journey",
+                description: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+            }
+        ]
+    },
+    footer: {
+        description: "Let’s work together on your next product.",
+        personalBlog: "Personal Blog"
+    }
 }
 
 export default english;
--- src/data/language/turkish.js
+++ src/data/language/turkish.js
@@ -1,5 +1,52 @@
 const turkish = {
-
+    modeSwitch: {
+        darkMode: "KARANLIK MOD",
+        switchLanguage: "Türkçeye Çevir"
+    },
+    header: {
+        skills: "Yetenekler",
+        projects: "Projeler",
+        hireMe: "Beni işe al"
+    },
+    hero: {
+        text1: "Yaratıcı düşünen Minimalizm aşığı",
+        text2: "Merhaba, ben Almila. Tam yığın geliştiriciyim. Eğer sağlam ve ölçeklenebilir ön uç ürünleri ile harika kullanıcı deneyimleri sunan bir geliştirici arıyorsanız. Benimle el sıkışalım.",
+        hireMeButton: "Beni işe al"
+    },
+    skills: {
+        title: "Yetenekler"
+    },
+    projects: {
+        title: "Profil",
+        profile: "Profil",
+        aboutMe: "Hakkımda",
+        dateOfBirth: ["Doğum Tarihi", "24.03.1996"],
+        livingIn: ["Yaşadığı Yer", "Ankara"],
+        education: ["Eğitim", "Hacettepe Üni. Biyoloji"],
+        preferredRole: ["Tercih Edilen Rol", "Ön Uç, UI"]
+    },
+    profile: {
+        title: "Projeler",
+        viewSite: "Siteyi Görüntüle",
+        projects: [
+            {
+                title: "Workintech",
+                description: "Kullanıcılarınızın hangi çerezleri kabul edip hangilerini reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimal bir kurulum çerez eklentisi. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, koda ve temalara herhangi bir türde özelleştirme yapılmasını mümkün kılar.",
+            },
+            {
+                title: "Random Jokes",
+                description: "Kullanıcılarınızın hangi çerezleri kabul edip hangilerini reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimal bir kurulum çerez eklentisi. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, koda ve temalara herhangi bir türde özelleştirme yapılmasını mümkün kılar.",
+            },
+            {
+                title: "Journey",
+                description: "Kullanıcılarınızın hangi çerezleri kabul edip hangilerini reddedeceklerini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimal bir kurulum çerez eklentisi. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, koda ve temalara herhangi bir türde özelleştirme yapılmasını mümkün kılar.",
+            }
+        ]
+    },
+    footer: {
+        description: "Bir sonraki ürününüzde birlikte çalışalım.",
+        personalBlog: "Kişisel Blog"
+    }
 }
 
 export default turkish;
