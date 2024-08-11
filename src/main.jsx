ea4ba814f2f1747b47333294dabb4e425ad9b388
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-11 18:06:40 +0300

    Wrap providers around App

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import DarkModeProvider from './context/darkmode/DarkModeProvider';
+import LanguageProvider from './context/language/LanguageProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <DarkModeProvider>
+        <LanguageProvider>
+            <App />
+        </LanguageProvider>
+    </DarkModeProvider>
 )
