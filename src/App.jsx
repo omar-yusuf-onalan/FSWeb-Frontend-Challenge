27943a3e97ea11db82e7a0a47287bbbfb788dd04
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-12 18:27:40 +0300

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import ModeSwitch from "./components/modeswitch/ModeSwitch.jsx";
+import Header from "./components/header/Header.jsx";
 
 const App = () => {
   return (
     <div>
       <ModeSwitch />
+        <Header />
     </div>
   )
 }
