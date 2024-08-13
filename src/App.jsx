84156da94a1f098c3b89c5f246400e089a4ff3f4
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-13 19:06:36 +0300

    Add completed components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import ModeSwitch from "./components/modeswitch/ModeSwitch.jsx";
 import Header from "./components/header/Header.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import Skills from "./components/skills/Skills.jsx";
+import Profile from "./components/profile/Profile.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 const App = () => {
-  return (
-    <div>
-      <ModeSwitch />
-        <Header />
-    </div>
-  )
+    return (
+        <div>
+            <ModeSwitch />
+            <Header />
+            <Hero />
+            <Skills />
+            <Profile />
+            <Projects />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
