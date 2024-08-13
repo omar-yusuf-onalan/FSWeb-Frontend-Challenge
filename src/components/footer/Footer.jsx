8a75f3c976c0d0810208784610569e9f235f3966
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-13 19:06:06 +0300

    Create HTML skeleton

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
-import styles from './Footer.module.css';
+import darkStyles from "./Footer.dark.module.css";
+import lightStyles from "./Footer.light.module.css";
+import useDarkModeContext from "../../context/darkmode/useDarkModeContext.jsx";
+import useLanguageContext from "../../context/language/useLanguageContext.jsx";
 
 const Footer = () => {
+    const { isDarkMode } = useDarkModeContext();
+
+    const styles = isDarkMode ? darkStyles : lightStyles;
+
+    const {language} = useLanguageContext();
+
     return (
         <div>
+            <div>
+                <div>
+                    <h1>{language.footer.description}</h1>
+                </div>
+            </div>
 
+            <div>
+                <span>👉 almilasucode@gmail.com</span>
+
+                <div>
+                    <span>{language.footer.personalBlog}</span>
+                    <span>Github</span>
+                    <span>Linkedin</span>
+                </div>
+            </div>
         </div>
     )
 }
