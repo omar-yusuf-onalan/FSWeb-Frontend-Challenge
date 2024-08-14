0135c5d577964e2f8523d78aa9b1d09ea5d17f59
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-14 09:09:33 +0300

    Add app and container classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import styles from './App.module.css';
 import ModeSwitch from "./components/modeswitch/ModeSwitch.jsx";
 import Header from "./components/header/Header.jsx";
 import Hero from "./components/hero/Hero.jsx";
@@ -7,16 +7,23 @@ import Profile from "./components/profile/Profile.jsx";
 import Projects from "./components/projects/Projects.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
+const {
+    app,
+    container
+} = styles;
+
 const App = () => {
     return (
-        <div>
-            <ModeSwitch />
-            <Header />
-            <Hero />
-            <Skills />
-            <Profile />
-            <Projects />
-            <Footer />
+        <div className={app}>
+            <div className={container}>
+                <ModeSwitch />
+                <Header />
+                <Hero />
+                <Skills />
+                <Profile />
+                <Projects />
+                <Footer />
+            </div>
         </div>
     )
 }
