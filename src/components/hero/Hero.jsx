1481334abccdd8ea9c34d771d57bd2aea124ec24
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-15 11:28:10 +0300

    Add dark mode styles for Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -21,26 +21,44 @@ const Hero = () => {
 
     const {language} = useLanguageContext();
 
+    const hireMeStyle = {
+        color: isDarkMode ? "#000000" : "#FFFFFF",
+        background: isDarkMode ? "#E1E1FF" : "#3730A3"
+    }
+
+    const githubStyle = {
+        color: isDarkMode ? "#E1E1FF" : "#3730A3",
+        background: isDarkMode ? "#383838" : "#FFFFFF",
+        border: isDarkMode ? "1px solid #E1E1FF" : "1px solid #3730A3"
+    }
+
+    const linkedinStyle = {
+        color: isDarkMode ? "#E1E1FF" : "#3730A3",
+        background: isDarkMode ? "#383838" : " #FFFFFF",
+        border: isDarkMode ? "1px solid #E1E1FF" : "1px solid #3730A3"
+    }
+
     return (
         <div className={hero}>
             <div className={left}>
 
                 <p className={name}><div></div> <span style={{color: isDarkMode ? "#B7AAFF" : "#4338CA"}}>Almila Su</span></p>
 
-                <p className={text1} style={{color: isDarkMode ? "" : "#1F2937"}}>{language.hero.text1}</p>
-                <p className={text2} style={{color: isDarkMode ? "" : "#6B7280;"}}>{language.hero.text2}</p>
+                <p className={text1} style={{color: isDarkMode ? "#AEBCCF" : "#1F2937"}}>{language.hero.text1}</p>
+                <p className={text2} style={{color: isDarkMode ? "#FFFFFF" : "#6B7280"}}>{language.hero.text2}</p>
 
                 <div className={buttons}>
 
-                    <div className={hireMe} style={{color: isDarkMode ? "" : "white",background: isDarkMode ? "" : "#3730A3"}}>
+                    <div
+                        className={hireMe} style={hireMeStyle}>
                         {language.hero.hireMeButton}
                     </div>
 
-                    <div className={github} style={{color: isDarkMode ? "" : "#3730A3", border: isDarkMode ? "" : "1px solid #3730A3"}}>
+                    <div className={github} style={githubStyle}>
                         Github
                     </div>
 
-                    <div className={linkedin} style={{color: isDarkMode ? "" : "#3730A3", border: isDarkMode ? "" : "1px solid #3730A3"}}>
+                    <div className={linkedin} style={linkedinStyle}>
                         Linkedin
                     </div>
                 </div>
