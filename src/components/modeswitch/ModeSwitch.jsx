345bb35777d5d927ac42c6fc818fa97cbc4ec617
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-19 18:58:52 +0300

    Add single css file

--- src/components/modeswitch/ModeSwitch.jsx
+++ src/components/modeswitch/ModeSwitch.jsx
@@ -1,13 +1,10 @@
 import useDarkModeContext from "../../context/darkmode/useDarkModeContext.jsx";
 import DarkMode from "../darkmode/DarkMode.jsx";
 import SelectLanguage from "../selectlanguage/SelectLanguage.jsx";
-import darkStyles from "./ModeSwitch.dark.module.css";
-import lightStyles from "./ModeSwitch.light.module.css";
+import styles from "./ModeSwitch.module.css";
 
 const ModeSwitch = () => {
     const { isDarkMode } = useDarkModeContext();
-
-    const styles = isDarkMode ? darkStyles : lightStyles;
 
     const {
         modeSwitch
